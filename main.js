6b372dc93b84f553fda28ecc59b6c535cb818ee3
Tomas <53576581+tomaschapot@users.noreply.github.com>
2022-03-02 17:11:27 -0300

    added calculator functions

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+//Global Variables
+let currentValue = "";
+let currentOperation = "";
+
 //DOM
+const $screen = document.querySelector(".screen");
+
+const $add = document.querySelector("add");
+const $substract = document.querySelector("substract");
+const $divide = document.querySelector("divide");
+const $multiply = document.querySelector("multiply");
+const $clear = document.querySelector(".clear");
+
+const $operationsList = document.querySelectorAll(".operation");
+const $numbers = document.querySelectorAll(".number");
+
+$numbers.forEach((number) => {
+	$screen.value = "";
+	number.addEventListener("click", () => {
+		$screen.value = $screen.value + number.innerText;
+	});
+});
 
 // Calculator Operations Functions
 
@@ -20,3 +41,30 @@ function divide(number1, number2) {
 function operate(number1, number2, operation) {
 	return operation(number1, number2);
 }
+
+function displayResult(operation) {
+	$screen.value = operation;
+}
+
+function clear() {
+	$clear.addEventListener("click", () => {
+		$screen.value = "";
+	});
+}
+
+clear();
+
+console.log($operationsList);
+
+$operationsList.forEach((operation) => {
+	operation.addEventListener("click", () => {
+		currentValue = $screen.value;
+		currentOperation = operation.getAttribute("id");
+		console.log(currentOperation);
+		console.log(currentValue);
+	});
+});
+
+// store number input when user presses an operator.
+// save chosen operation to pass on operate
+// when the user presses =, pass the second number and operate.
