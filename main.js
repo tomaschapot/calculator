b3d2bcc964adda543cd8a323de665f626443f617
Tomas <53576581+tomaschapot@users.noreply.github.com>
2022-03-02 18:11:47 -0300

    calculator operator updated

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //Global Variables
 let currentValue = "";
+let nextValue = "";
 let currentOperation = "";
+let currentOperationSimbol = "";
 
 //DOM
 const $screen = document.querySelector(".screen");
@@ -10,36 +12,38 @@ const $substract = document.querySelector("substract");
 const $divide = document.querySelector("divide");
 const $multiply = document.querySelector("multiply");
 const $clear = document.querySelector(".clear");
-
+const $equal = document.querySelector(".equal");
 const $operationsList = document.querySelectorAll(".operation");
 const $numbers = document.querySelectorAll(".number");
-
-$numbers.forEach((number) => {
-	$screen.value = "";
-	number.addEventListener("click", () => {
-		$screen.value = $screen.value + number.innerText;
-	});
-});
+const $dotButton = document.querySelector(".dot");
 
 // Calculator Operations Functions
 
 function add(number1, number2) {
-	return number1 + number2;
+	return Number(number1) + Number(number2);
 }
 function substract(number1, number2) {
-	return number1 - number2;
+	return Number(number1) - Number(number2);
 }
 function multiply(number1, number2) {
-	return number1 * number2;
+	return Number(number1) * Number(number2);
 }
 function divide(number1, number2) {
-	return number1 / number2;
+	return Number(number1) / Number(number2);
 }
 
 //Main Operator function
 
-function operate(number1, number2, operation) {
-	return operation(number1, number2);
+function operate(number1, number2, operationSimbol) {
+	if (operationSimbol === "+") {
+		return add(number1, number2);
+	} else if (operationSimbol === "/") {
+		return divide(number1, number2);
+	} else if (operationSimbol === "*") {
+		return multiply(number1, number2);
+	} else {
+		return substract(number1, number2);
+	}
 }
 
 function displayResult(operation) {
@@ -49,21 +53,52 @@ function displayResult(operation) {
 function clear() {
 	$clear.addEventListener("click", () => {
 		$screen.value = "";
+		currentValue = 0;
+		nextValue = 0;
 	});
 }
 
-clear();
+function buttonsInput() {
+	$numbers.forEach((number) => {
+		$screen.value = "";
+		number.addEventListener("click", () => {
+			$screen.value = $screen.value + number.innerText;
+		});
+	});
+}
 
-console.log($operationsList);
+function operationInput() {
+	$operationsList.forEach((operation) => {
+		operation.addEventListener("click", () => {
+			currentValue = $screen.value;
+			currentOperation = operation.getAttribute("id");
+			currentOperationSimbol = operation.querySelector("p").innerText;
+			$screen.value = `${currentValue}${currentOperationSimbol}`;
+			console.log(currentOperationSimbol);
+			console.log(currentValue);
+		});
+	});
+}
 
-$operationsList.forEach((operation) => {
-	operation.addEventListener("click", () => {
-		currentValue = $screen.value;
-		currentOperation = operation.getAttribute("id");
+function equalInput() {
+	$equal.addEventListener("click", () => {
+		let input = $screen.value.split(currentOperationSimbol);
+		nextValue = input[1];
 		console.log(currentOperation);
 		console.log(currentValue);
+		console.log(nextValue);
+		$screen.value = operate(currentValue, nextValue, currentOperationSimbol);
 	});
-});
+}
+
+function calculus() {
+	clear();
+	buttonsInput();
+	operationInput();
+	equalInput();
+}
+
+calculus();
 
 // store number input when user presses an operator.
 // save chosen operation to pass on operate
